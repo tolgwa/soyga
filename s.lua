d4ab1a7572695169f221738bec52b54e570ed5a5
tolgwa <tolga.serbest1@gmail.com>
2026-03-30 19:22:45 +0300

    soyga.lua

--- s.lua
+++ s.lua
@@ -9,17 +9,18 @@ local r = game:GetService("RunService")
 task.spawn(function()
     local VirtualUser = game:GetService('VirtualUser')
 
-    p.Idled:Connect(function()
+    game:GetService('Players').LocalPlayer.Idled:Connect(function()
         VirtualUser:CaptureController()
         VirtualUser:ClickButton2(Vector2.new())
     end)
 
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "AntiAFK loaded!",
+        Text = "Made by Gio",
+        Button1 = "Thanks",
         Duration = 5
     })
 end)
-
 -------------------------------------------------
 -- Noclip GUI
 -------------------------------------------------
